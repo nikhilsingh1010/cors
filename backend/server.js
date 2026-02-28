a81444af7e88dbe071ec8422bc2115ea39275397
nikhilsingh1010 <nikhilsinghthakur1010@gmail.com>
2026-02-28 23:18:50 +0530

    Initial commit: frontend and backend setup

---
+++ backend/server.js
@@ -0,0 +1,46 @@
+import express from 'express';
+
+const app = express();
+
+// app.get('/', (req, res) => {
+//     res.send('Hello from the backend!');
+// });
+
+//get a list of 5 jokes
+app.get('/api/jokes', (req, res) => {
+    const jokes = [
+        {
+            id: 1,
+            title: 'A joke',
+            content: 'this is a joke'
+        },
+        {
+            id: 2,
+            title: 'Another joke',
+            content: 'this is another joke'
+        },
+        {
+            id: 3,
+            title: 'Yet another joke',
+            content: 'this is yet another joke'
+        },
+        {
+            id: 4,
+            title: 'A fourth joke',
+            content: 'this is a fourth joke'
+        },
+        {
+            id: 5,
+            title: 'A fifth joke',
+            content: 'this is a fifth joke'
+        }
+    ];
+    res.send(jokes);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=> {
+    console.log(`Server at http://localhost:${port}`);  
+
+})
